7449146f61bd7fb6ad5c399570f9ba05260060b8
Sid <test>
2024-05-01 19:46:31 +0530

    Some more of Exp 5 left

--- Experiments/Exp5/inference.py
+++ Experiments/Exp5/inference.py
@@ -2,7 +2,7 @@ import torch
 import torch
 import torch.nn as nn
 import torch.optim as optim
-
+THRESHOLD =0.5
 class MusicLSTM(nn.Module):
     def __init__(self, input_size, hidden_size, num_layers, output_size):
         super(MusicLSTM, self).__init__()
@@ -26,16 +26,44 @@ class MusicLSTM(nn.Module):
 
         return torch.cat(outputs, dim=1)
     
-
+import librosa
+import numpy as np
 class ThreeTierEnsemble():
     def __init__(self,model30path,model10path,model1path):
+        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
         self.model30 = torch.load(model30path)
         self.model10 = torch.load(model10path)
         self.model1 = torch.load(model1path)
-    def predict(self,x):
-        out30 = self.model30(x).detach().cpu().numpy()[0] > 0.5
-        # for i,x in enumerate(x):
+        self.model30.to(self.device)
+        self.model10.to(self.device)
+        self.model1.to(self.device)
+    def extract_mfcc(self,y, sr=22050, n_mfcc=13, n_fft=2048, hop_length=512, duration=5):
+        num_segments = int(len(y) / (duration * sr))
+        mfccs = []
+        for i in range(num_segments):
+            segment = y[i * duration * sr : (i + 1) * duration * sr]
+            mfcc = librosa.feature.mfcc(y=segment, sr=sr, n_mfcc=n_mfcc, n_fft=n_fft, hop_length=hop_length)
+            mfcc_mean = np.mean(mfcc, axis=1)
+            mfccs.append(mfcc_mean)
+
+        return np.stack(mfccs, axis=0)
+    def predict(self,y):
+        x = self.extract_mfcc(y,duration=30)
+        out30 = list(self.model30(torch.tensor(x,dtype=torch.float).to(self.device).detach().cpu().numpy()[0] >= THRESHOLD))
+        nextinp = []
+        nx = self.extract_mfcc(y,duration=10)
+        for i,data in enumerate(x):
+            if out30[i]:
+                nextinp.append(nx[(i*3):(i*3)+3])
+            else:
+                nextinp.append([[0]*13]*3)
+        nextinp=torch.tensor(np.array(nextinp))
+        #-----TODO-----
 
 
-# -------TODO--------------
+
+                
+
+
+
 
